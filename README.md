daf9bc437ede71b24ae189731fe16c645fbdbcfb
Erik VanderWerf <erik.vanderwerf@dell.com>
2020-07-20 18:26:00 -0700

    Adding some basic config files. Some things still need to be configured.
    App is using gunicorn.

--- README.md
+++ README.md
@@ -2,4 +2,7 @@
 
 This plugin example uses a Python/Flask back end and a JavaScript/React front end.
 
-The front end code lives under `example/static/`.
+The front end code lives under `/static/`.
+
+## Binning
+`bin = api.bin("/virtual/path/")`
--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from os import getcwd
+import os
+
+from dataiq.plugin.context import Context
 from dataiq.plugin.user import HardcodedAdminUser
-from example import Example
 from flask import render_template
 
-AUTH_OVERRIDE = HardcodedAdminUser('override')
+from example import Example
+
+AUTH_OVERRIDE = os.getenv('AUTH_OVERRIDE')
+AUTH_OVERRIDE = None if AUTH_OVERRIDE is None else HardcodedAdminUser(AUTH_OVERRIDE)
 
 
 app = Example(AUTH_OVERRIDE)
@@ -17,6 +21,17 @@ def index():
 @app.route('/hello')
 def hello():
     return 'Hello from Flask'
+
+
+@app.action('/execute')
+def execute(context: Context):
+    print(context)
+
+
+@app.route('/bins', methods=['GET', 'POST'])
+def bins():
+    user = app.require_user()
+    return str(user)
 
 
 if __name__ == '__main__':
