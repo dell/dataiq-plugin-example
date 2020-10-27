a25d0d9b08abf44fcd74db91b2ea4a77c42078d2
Eric Jackson <eric.jackson@emc.com>
2020-10-27 10:55:59 -0700

    Remove unused test util

--- static/src/__tests__/Main.test.js
+++ static/src/__tests__/Main.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen, wait } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import { MuiPickersUtilsProvider } from '@material-ui/pickers';
 import MomentUtils from '@date-io/moment';
